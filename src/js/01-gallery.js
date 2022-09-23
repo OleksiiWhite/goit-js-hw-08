a8bb2b139693a58c36d462705725bf4e658bbc0d
Oleksii Biletskyi <alenikbel@ukr.net>
2022-09-23 16:40:23 +0300

    task 1 modul 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const addGalleryHtml = createGalleryHtml(galleryItems);
+
+function createGalleryHtml(items) {
+  return items
+    .map(
+      item =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>`
+    )
+    .join('');
+}
+
+galleryRef.innerHTML = addGalleryHtml;
+
+const galleryOn = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionDelay: 250,
+  overlayOpacity: 0.3,
+});
+
+galleryOn.on('show.simplelightbox', function (e) {
+  `<img src = "${e.target.dataset.source}" width = "800" height = "600">`;
+});
